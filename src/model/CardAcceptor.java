e935d8c97d89d6427efa37eccd4e4a7dc242f3b7
Dzholdoshbev Chyngyz <medzholdoshbaev@gmail.com>
2024-04-25 12:48:01 +0600

    created getter and setter for private int amount

--- src/model/CardAcceptor.java
+++ src/model/CardAcceptor.java
@@ -5,4 +5,11 @@ public class CardAcceptor {
     public CardAcceptor(int amount){
         this.amount = amount;
     }
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
